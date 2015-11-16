0df2b328f2a6c7c9596187fe6330dfce969ab69e
Derek Haynes <dhaynes@redhat.com>
2015-11-16 12:43:31 +0100

    Using interfaces/DI

---
+++ src/main/java/org/esbtools/gateway/resync/ResyncServiceBase.java
@@ -0,0 +1,11 @@
+package org.esbtools.gateway.resync;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by dhaynes on 11/16/15.
+ */
+public abstract class ResyncServiceBase implements ResyncService {
+    protected static final Logger LOGGER= LoggerFactory.getLogger(ResyncResponse.class);
+}
